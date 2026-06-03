b0b510e66d1a0c1fa6a65895c528268da276e93c
alvin <limithunter1@gmail.com>
2026-06-03 23:28:04 +0700

    feat: add categories and incomes navigation links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,11 +10,32 @@
                     </a>
                 </div>
 
+                <div class="shrink-0 flex items-center">
+                    <a href="{{ route('categories.index') }}">
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    </a>
+                </div>
+
+                <div class="shrink-0 flex items-center">
+                    <a href="{{ route('incomes.index') }}">
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    </a>
+                </div>
+
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('categories.index')" :active="request()->routeIs('categories')">
+                        {{ __('Categories') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('incomes.index')" :active="request()->routeIs('incomes')">
+                        {{ __('Incomes') }}
+                    </x-nav-link>
+
                 </div>
             </div>
 
